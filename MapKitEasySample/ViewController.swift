fc77db52e094827dfe59171f7cf9559f2b3750ac
tomotakashimizu <tomotaka.developer@gmail.com>
2020-09-21 21:48:13 +0900

    Initial Commit

---
+++ MapKitEasySample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapKitEasySample
+//
+//  Created by 清水智貴 on 2020/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
